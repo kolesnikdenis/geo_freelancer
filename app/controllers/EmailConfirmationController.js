2d81971db73d075d136ff7cfd8f58a6ff70e54c3
Darilana <darilana.kudinova@gmail.com>
2018-02-11 15:30:00 +0200

    email confirmation logic added

--- app/controllers/EmailConfirmationController.js
+++ app/controllers/EmailConfirmationController.js
@@ -1,13 +1,26 @@
-angular.module('app').controller('EmailConfirmationController', function($routeParams, UserService) {
+angular.module('app').controller('EmailConfirmationController', function($rootScope, $routeParams, UserService, AuthService) {
+    const self = this;
+    self.showSuccess = false;
+    self.showError = false;
     const confirmationData = {
         username: $routeParams.username,
         token: $routeParams.token,
     };
     UserService.confirm(confirmationData)
-        .then(resp => {
-
+        .then(response => {
+            if (response.data.status ==='ok') {
+                AuthService.saveAuthData(confirmationData.token, confirmationData.username);
+                $rootScope.$broadcast('authenticated');
+                self.showSuccess = true;
+                self.showError = false;
+            }
+            else {
+                self.showSuccess = false;
+                self.showError = true;
+            }
         })
-        .catch(err => {
-
+        .catch(error => {
+            self.showSuccess = false;
+            self.showError = true;
     });
 });
