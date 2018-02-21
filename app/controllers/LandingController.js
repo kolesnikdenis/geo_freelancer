cb68dddf0dc06194c40f552b939e0217bd17abb0
Kolesnik Denis <i@kolesnikdenis.com>
2018-02-21 04:07:03 +0200

    fix carousel, add directives fixImgErrorSrc

--- app/controllers/LandingController.js
+++ app/controllers/LandingController.js
@@ -3,7 +3,26 @@ angular.module('app').controller('LandingController', function($scope, LandingSe
     $scope.category={};
     LandingService.getAll().then((category) => {
         $scope.category = category.data.response;
-        console.log($scope.category)
-        category.data.response.map( function (item){ console.log(item.icon)})
-    })
+        $(document).ready(function(){
+            $(".owl-carousel").owlCarousel({
+                items:4,
+                loop:true,
+                margin:10,
+                nav:false,
+                responsiveClass:true,
+                merge: true,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:2
+                    },
+                    1000:{
+                        items:3
+                    }
+                }
+            });
+        });
+    });
 });
