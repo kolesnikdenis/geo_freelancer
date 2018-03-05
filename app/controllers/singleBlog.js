af3fd6238ec1e1a12706fbd7f5028cc2be664ff8
Darilana <darilana.kudinova@gmail.com>
2018-03-05 17:16:49 +0200

    blog changes (added by Maxim)

--- app/controllers/singleBlog.js
+++ app/controllers/singleBlog.js
@@ -1,5 +1,4 @@
-angular.module('app').controller('singleBlog', function($http, $scope, $routeParams) {
-
+angular.module('app').controller('singleBlog', function($http, $scope, $routeParams, BlogService, UserService, notify) {
 
   $scope.blogs =  [
             {
@@ -11,8 +10,6 @@ angular.module('app').controller('singleBlog', function($http, $scope, $routePar
 
             }
           ];
-
-
 
           $http.get('http://freelance.kolesnikdenis.com/api/blog').then(function (responce){
                 $scope.blogs=responce.data.blog_array;
@@ -27,12 +24,9 @@ angular.module('app').controller('singleBlog', function($http, $scope, $routePar
           );
 
 
-
-
-
-
-          $http.get('//freelance.kolesnikdenis.com/api/blog_comment/1')
+          $http.get('//freelance.kolesnikdenis.com/api/blog_comment/'+$routeParams.postId)
           .then(function (responce){
+            console.log(responce)
                 $scope.comments=responce.data.comment_blog;
 
               }, function error(response){
@@ -41,42 +35,54 @@ angular.module('app').controller('singleBlog', function($http, $scope, $routePar
           );
 
 
-            $scope.comment =  {
-                  id: "",
-                  datetime: "",
-                  user_id: "",
-                  blog_id: "",
-                  msg: "",
-                  firstname: "",
-                  surname: "",
+          UserService.requestUserInfo().then((my_info)=>{
+                if (my_info.data.status=="ok"){
+                    if (my_info.data.user_profile.photos ) {
+                        my_info.data.user_profile.photos = JSON.parse(my_info.data.user_profile.photos);
+                        if (my_info.data.user_profile.photos[0] && my_info.data.user_profile.photos[0].filename)
+                            my_info.data.user_profile.photos = "/upload/"+my_info.data.user_profile.photos[0].filename;
+                        else
+                            my_info.data.user_profile.photos  = "http://icons.iconarchive.com/icons/tristan-edwards/sevenesque/1024/Preview-icon.png";
 
+                    }
+                    this.my_info=my_info.data.user_profile;
+                }else {
+                    console.log("вы не зареганы")
                 }
-
-                $scope.addComment = function (comment) {
-                              console.log(comment)
-                              $http.post('//freelance.kolesnikdenis.com/api/blog_comment', comment)
-                              .then(function (responce){
-                                  console.log('comment successfully saved')
-
-                                  $scope.comment = null;
-                                  }
-                              );
-
-                            };
+            },(err)=>{
+                //console.log();
+                notify({ message: "Вы не авторизованы, и по этой причине вы не можете писать комментарии", duration: 3000, classes: 'alert alert-danger'  });
+            });
 
 
+    $scope.showblogcontent= $routeParams.postId;
+    $scope.comments=[];
 
+        $scope.constructor_comment={blog_id:$scope.showblogcontent, msg:""};
+        $scope.send= function(){
+          
+            if (!$scope.constructor_comment.msg || $scope.constructor_comment.msg.length <=0 ) {
+                notify({ message: "сообщение не может быть пустым", duration: 3000, classes: 'alert alert-danger'  });
+            } else {
+                BlogService.addComment($scope.constructor_comment).then((req) => {
+                    if (req.data.status=="ok"){
+                        notify({ message: "Спасибо, Ваш комментарий успешно добавлен", duration: 3000, classes: 'alert alert-info'  });
+                        
+                        $scope.comments.push({datetime: new Date(), firstname:this.my_info.firstname, msg:$scope.constructor_comment.msg,surname:this.my_info.surname,user_id:this.my_info.id});
+                        console.log($scope.comments);
+                        $scope.constructor_comment.msg="";
+                    }else {
+                        notify({ message: "произошла ошибка", duration: 3000, classes: 'alert alert-danger'  });
+                    }
 
-
-
-
-
+                    console.log(req);
+                });
+            }
+        }
 
   $scope.show = 'msg1';
   $scope.fullContact = function () {
   $scope.show = $scope.show == 'msg1' ? 'msg2' : 'msg1'; };
-
-
 
 console.log($routeParams.postId)
 
